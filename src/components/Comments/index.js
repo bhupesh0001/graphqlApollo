a01e290812b09ca4589241081c778974f082e5e0
Bhupesh <bhupesh@sap.com>
2019-01-07 14:54:03 +0530

    Comment Component

---
+++ src/components/Comments/index.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import CommentItem from './Item';
+import styles from './style';
+
+export default class Comments extends PureComponent {
+  render() {
+    const { items } = this.props;
+    return (
+      <View style={styles.container}>
+        {items.map(comment => (
+          <CommentItem item={comment} style={{ flex: 1 }} key={comment.id} />
+        ))}
+      </View>
+    );
+  }
+}
+Comments.propTypes = {
+  items: PropTypes.array.isRequired,
+};
