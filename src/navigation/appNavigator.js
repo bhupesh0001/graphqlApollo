06d2a86a27aca401fbb7f21b34ad78b36c3a4b9f
Bhupesh <bhupesh@sap.com>
2019-01-07 15:05:33 +0530

    AppNavigator for app navigation

---
+++ src/navigation/appNavigator.js
@@ -0,0 +1,30 @@
+import {
+  createAppContainer,
+  createStackNavigator,
+} from 'react-navigation';
+import { Platform } from 'react-native';
+
+import HackerNewsDetails from '../screens/HackerNews/Details';
+import HackerNewsList from '../screens/HackerNews/List';
+
+const AppNavigator = createAppContainer(
+  createStackNavigator(
+    {
+      HackerNewsList,
+      HackerNewsDetails,
+      Index: {
+        screen: HackerNewsList,
+      },
+    },
+    {
+      initialRouteName: 'Index',
+      headerMode: 'none',
+      /*
+       * Use modal on iOS because the card mode comes from the right
+       */
+      mode: Platform.OS === 'ios' ? 'modal' : 'card',
+    },
+  ),
+);
+
+export default AppNavigator;
